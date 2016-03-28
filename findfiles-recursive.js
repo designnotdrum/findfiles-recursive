3a14ba0f23a3d236d916a4aec76c60f08c30888a
Nick Mason <nick.mason@maxpoint.com>
2016-03-28 15:11:17 -0400

    I AM A GOD AMONGST MEN

--- findfiles-recursive.js
+++ findfiles-recursive.js
@@ -3,34 +3,32 @@ var readdirp = require('readdirp'),
 	es = require('event-stream'),
 	fs = require('fs');
 
-// print out all JS files to console
-
-var datArrayDoe = [];
+// print out all JS files to file
 
 var makeMyList = fs.createWriteStream('filelist.json');
 
-var stream = readdirp({ 
-	root: path.join(__dirname), 
-	fileFilter: '*.js', 
+var stream = readdirp({
+	root: path.join(__dirname),
+	fileFilter: '*.js',
 	directoryFilter: [
-		'!.git', 
-		'!*modules', 
-		'!bower*' 
-	] 
+		'!.git',
+		'!*modules',
+		'!bower*'
+	]
 });
 
 stream
 	.on('data', function (entry) {
-		datArrayDoe.push({ name: entry.name, path: entry.parentDir });
+		console.log("Found a file named "+ entry.name + " at " + entry.path);
 	})
 	.on('warn', function (err) {
 		console.error('non-fatal error', err);
 	})
 	.on('error', function (err) {
 		console.error('shit\'s all fuckt up, mane', err);
-	})	
-	//.pipe(es.stringify(datArrayDoe))
+	})
 	.pipe(es.mapSync(function (entry) {
-		return JSON.stringify({ name: entry.parentDir, path: entry.path }, null, '\t');
+		return JSON.stringify([{filename: entry.name , friendlyName: entry.path , filePath: entry.path}], null, '\t');
 	}))
+	.pipe(es.join(","))
 	.pipe(makeMyList);
